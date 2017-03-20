314c2227e0082cedb52d6e6b917dd086613f0547
Carlos A <carlos.ar.mercado@gmail.com>
2017-03-20 13:36:16 -0700

    Importing project

---
+++ Assets/Movement.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+
+    public float magnitude = 1;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    void ProcessInput()
+    {
+        Vector3 force = new Vector3(0,0,0);
+
+        if(Input.GetKey(KeyCode.W))
+        {
+            force += new Vector3(0, 0, magnitude);
+        }
+
+        gameObject.GetComponent<Rigidbody>().AddForce(force);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+}
